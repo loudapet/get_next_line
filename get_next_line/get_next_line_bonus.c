3e1bd2a67a42837d215150bece2230bf9dea34f4
Petr Louda <plouda@c1r10s4.42prague.com>
2023-02-07 13:56:20 +0100

    Edit header names for submission

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*set_next_line(char *text)
 {
